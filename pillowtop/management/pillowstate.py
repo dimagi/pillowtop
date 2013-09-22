489611e93cc2ce53b843a2eb174ce5aa123e8f7a
dmyung <dmyung@dimagi.com>
2013-09-21 22:59:17 -0400

    adding more accurate wya to inspect master pillow/alias states

--- pillowtop/management/pillowstate.py
+++ pillowtop/management/pillowstate.py
@@ -1,45 +1,49 @@
+import simplejson
 from corehq.elastic import get_es
 from pillowtop.listener import AliasedElasticPillow
 
 
 def get_pillow_states(pillows):
-    aliased_pillows = filter(lambda x: isinstance(x, AliasedElasticPillow), pillows)
-    #make tuples of (index, alias)
-    #this maybe problematic in the future if we have multiple pillows pointing to the same alias or indices
-    master_aliases = dict((x.es_index, x.es_alias) for x in aliased_pillows)
+    """
+    Return states of all pillows
 
+    mapped_masters: current indices that are correctly aliased
+    unmapped_masters: current indices that are not yet aliased
+    stale_indices: indices that are not master but may or may not have an active alias
+    """
     es = get_es()
     system_status = es.get('_status')
-    indices = system_status['indices'].keys()
-    print ""
-    print "\tActive indices on ES"
-    for index in indices:
-        print "\t\t%s" % index
-    print ""
 
+    aliased_pillows = filter(lambda x: isinstance(x, AliasedElasticPillow), pillows)
+    master_aliases = dict((x.es_index, x.es_alias) for x in aliased_pillows)
+
+    #all live indices on ES
+    active_indices = system_status['indices'].keys()
     active_aliases = es.get('_aliases')
 
-    mapped_masters = []
-    unmapped_masters = []
-    stale_indices = []
+    def has_alias(target_alias, alias_dict):
+        return target_alias in alias_dict
 
-    for idx, alias_dict in active_aliases.items():
-        line = ["\t\t", idx]
-        is_master = False
-        if idx in master_aliases:
-            is_master = True
+    #check if masters are ok
+    nonexistent_masters = set(master_aliases.keys()) - set(active_indices)
+    active_master_indices = set(master_aliases.keys()).intersection(active_indices)
+    active_masters = set()
+    inactive_masters = set()
 
-        if is_master:
-            if master_aliases[idx] in alias_dict['aliases']:
-                #is master, has alias, good
-                mapped_masters.append((idx, master_aliases[idx]))
-            else:
-                #is master, but doesn't have alias, bad
-                unmapped_masters.append((idx, master_aliases[idx]))
-                line.append('=> Does not have alias yet :(')
+    for am in active_master_indices:
+        target_alias = master_aliases[am]
+        if has_alias(target_alias, active_aliases.get(am, {}).get('aliases', {})):
+            active_masters.add(am)
         else:
-            #not a master index
-            stale_tuple = (idx, alias_dict['aliases'].keys())
-            stale_indices.append(stale_tuple)
+            inactive_masters.add(am)
+
+    stale_indices = []
+    non_master_indices = set(active_indices) - set(master_aliases.keys())
+    for nm in non_master_indices:
+        aliases_for_nm = active_aliases.get(nm, {}).get('aliases', {}).keys()
+        stale_indices.append((nm, aliases_for_nm))
+
+    mapped_masters = [(x, master_aliases[x]) for x in active_masters]
+    unmapped_masters = [(x, master_aliases[x]) for x in inactive_masters.union(nonexistent_masters)]
 
     return mapped_masters, unmapped_masters, stale_indices
